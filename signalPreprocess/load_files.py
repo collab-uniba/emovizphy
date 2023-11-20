48035482bdca6e8c6eeb32ecc7015bab1e358137
gianvitoperagine <gianvito.peragine@hotmail.it>
2023-11-20 23:38:39 +0100

    adapted to new library versions

--- signalPreprocess/load_files.py
+++ signalPreprocess/load_files.py
@@ -23,7 +23,7 @@ def _loadSingleFile_E4(data, list_of_columns, expected_sample_rate, freq):
 
     # Get the startTime and sample rate
     startTime = pd.to_datetime(float(data.columns.values[0]), unit="s")
-    sampleRate = float(data.iloc[0][0])
+    sampleRate = float(data.iloc[0].iloc[0])
     data = data[data.index != 0]
     data.index = data.index - 1
 
